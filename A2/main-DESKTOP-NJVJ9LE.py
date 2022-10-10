db1285154f1c63d98e5a06cf9c868cfb07770e06
WilliamEskildsen <107248266+WilliamEskildsen@users.noreply.github.com>
2022-10-10 16:28:19 +0200

    Delete main-DESKTOP-NJVJ9LE.py

--- A2/main-DESKTOP-NJVJ9LE.py
+++
@@ -1,5 +0,0 @@
-# Hi :) if just starting out - probably should start with the duplex.py this main.py references models that aren't shared on this repo
-
-import HTMLBuild as hb
-
-hb.modelLoader("duplex")
